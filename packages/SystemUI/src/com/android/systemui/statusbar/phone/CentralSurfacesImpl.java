LMODroid/platform_frameworks_base

SystemUI: improve blur handling in respect to app launch animations
Once the animation starts, AOSP just disables blur. This works because
the QS panel is not transparent, so the user won't notice. Due to our
transparent QS panel patches, it becomes obvious that blur just
disappears once a user opens an app from QS (long press qs tile, use
settings button, etc). To improve this situation, we use a small trick:
Unlike how it looks in the animation, the shade is always on top of
apps, hence, our newly-launched app is below the shade and below the
blur. We disable blur in the very moment the app starts fading in to make
it as un-noticeable as possible for the user, without accidentally
blurring the app in the process.

Change-Id: Iecd794ef67c92a76f93c500e8feec9723f0e0d41

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -4490,8 +4490,14 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
     private final ActivityLaunchAnimator.Listener mActivityLaunchAnimatorListener =
             new ActivityLaunchAnimator.Listener() {
                 @Override
-                public void onLaunchAnimationStart() {
-                    mKeyguardViewMediator.setBlursDisabledForAppLaunch(true);
+                public void onLaunchAnimationProgress(float progress) {
+                    // as per ActivityLaunchAnimator.kt TIMINGS object,
+                    // contentBeforeFadeOutDuration of 150ms and total duration of
+                    // totalDuration of 500ms means that fade out starts at 150 / 500 = 0.3f
+                    // don't call in if progress is 0.4f+ as microoptimization, it's already set
+                    if (progress >= 0.3f && progress < 0.4f) {
+                        mKeyguardViewMediator.setBlursDisabledForAppLaunch(true);
+                    }
                 }
 
                 @Override

Garnet-X/frameworks_base-derp

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -3295,8 +3295,14 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     private final ActivityTransitionAnimator.Listener mActivityTransitionAnimatorListener =
             new ActivityTransitionAnimator.Listener() {
                 @Override
-                public void onTransitionAnimationStart() {
-                    mKeyguardViewMediator.setBlursDisabledForAppLaunch(true);
+                public void onTransitionAnimationProgress(float progress) {
+                    // as per ActivityLaunchAnimator.kt TIMINGS object,
+                    // contentBeforeFadeOutDuration of 150ms and total duration of
+                    // totalDuration of 500ms means that fade out starts at 150 / 500 = 0.3f
+                    // don't call in if progress is 0.4f+ as microoptimization, it's already set
+                    if (progress >= 0.3f && progress < 0.4f) {
+                        mKeyguardViewMediator.setBlursDisabledForAppLaunch(true);
+                    }
                 }
 
                 @Override